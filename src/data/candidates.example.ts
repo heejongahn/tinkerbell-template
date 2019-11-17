301ff2674bc5aac40d8703d3319b21aa7158ef21
Heejong Ahn <heejongahn@gmail.com>
2019-11-17 12:52:32 +0900

    Add Filter.buildingType

--- src/data/candidates.example.ts
+++ src/data/candidates.example.ts
@@ -1,4 +1,4 @@
-import { Filter, RoomFloor, RoomCount, ContractType } from "../filter";
+import { Filter, RoomFloor, RoomCount, ContractType } from '../filter';
 
 /**
  * 월세 한도 (단위 만원)
@@ -10,45 +10,46 @@ const rentBudget = 100;
  */
 const depositBudget = 100;
 
-const commonFilter: Omit<Filter, "bounds" | "id"> = {
-  priceRange: {
-    rent: { max: rentBudget * 10000 },
-    deposit: { max: depositBudget * 10000 },
-    shouldIncludeMaintenance: true,
-  },
-  roomFloors: [RoomFloor.lower, RoomFloor.higher],
-  roomCounts: [RoomCount.twoRooms, RoomCount.threeAndMoreRooms],
-  contractTypes: [ContractType.rent],
-  shouldIncludeHalfUndergrounds: false,
-  shouldIncludeLofts: true,
-  shouldIncludeRooftops: true,
+const commonFilter: Omit<Filter, 'bounds' | 'id'> = {
+    priceRange: {
+        rent: { max: rentBudget * 10000 },
+        deposit: { max: depositBudget * 10000 },
+        shouldIncludeMaintenance: true,
+    },
+    buildingTypes: [],
+    roomFloors: [RoomFloor.lower, RoomFloor.higher],
+    roomCounts: [RoomCount.twoRooms, RoomCount.threeAndMoreRooms],
+    contractTypes: [ContractType.rent],
+    shouldIncludeHalfUndergrounds: false,
+    shouldIncludeLofts: true,
+    shouldIncludeRooftops: true,
 };
 
 const candidates: Filter[] = [
-  {
-    id: "뚝섬 서울숲",
-    ...commonFilter,
-    bounds: {
-      max: { lat: 37.5558485, lng: 127.060802 },
-      min: { lat: 37.5317832, lng: 127.0328288 },
+    {
+        id: '뚝섬 서울숲',
+        ...commonFilter,
+        bounds: {
+            max: { lat: 37.5558485, lng: 127.060802 },
+            min: { lat: 37.5317832, lng: 127.0328288 },
+        },
     },
-  },
-  {
-    id: "양재",
-    ...commonFilter,
-    bounds: {
-      max: { lat: 37.4854867, lng: 127.0506948 },
-      min: { lat: 37.4667919, lng: 127.0319895 },
+    {
+        id: '양재',
+        ...commonFilter,
+        bounds: {
+            max: { lat: 37.4854867, lng: 127.0506948 },
+            min: { lat: 37.4667919, lng: 127.0319895 },
+        },
     },
-  },
-  {
-    id: "회사 근처",
-    ...commonFilter,
-    bounds: {
-      max: { lat: 37.508058, lng: 127.0463052 },
-      min: { lat: 37.4893626, lng: 127.0275955 },
+    {
+        id: '회사 근처',
+        ...commonFilter,
+        bounds: {
+            max: { lat: 37.508058, lng: 127.0463052 },
+            min: { lat: 37.4893626, lng: 127.0275955 },
+        },
     },
-  },
 ];
 
 export default candidates;
--- src/filter.ts
+++ src/filter.ts
@@ -1,90 +1,93 @@
 interface PriceRange {
-  min?: number;
-  max: number;
+    min?: number;
+    max: number;
+}
+
+export enum BuildingType {
+    villa = '빌라/주택',
+    officetel = '오피스텔',
+    apt = '아파트',
+    office = '상가/사무실',
 }
 
 export enum RoomFloor {
-  lower = "1층 ~ 5층",
-  higher = "6층 이상",
+    lower = '1층 ~ 5층',
+    higher = '6층 이상',
 }
 
 export enum RoomCount {
-  oneRoom = "원룸",
-  twoRooms = "투룸",
-  threeAndMoreRooms = "쓰리룸 이상",
+    oneRoom = '원룸',
+    twoRooms = '투룸',
+    threeAndMoreRooms = '쓰리룸 이상',
 }
 
 export enum ContractType {
-  rent = "월세",
-  jeonse = "전세",
-  sale = "매매",
-  shortTerm = "단기임대",
+    rent = '월세',
+    jeonse = '전세',
+    sale = '매매',
+    shortTerm = '단기임대',
 }
 
 interface Point {
-  lat: number;
-  lng: number;
+    lat: number;
+    lng: number;
 }
 
 export interface Filter {
-  id: string;
-  priceRange: {
-    deposit?: PriceRange;
-    rent?: PriceRange;
-    /**
-     * 월세 한도 판단에 관리비를 포함할지 여부
-     */
-    shouldIncludeMaintenance: boolean;
-  };
-  bounds: {
-    max: Point;
-    min: Point;
-  };
-  roomFloors: RoomFloor[];
-  roomCounts: RoomCount[];
-  contractTypes: ContractType[];
-  shouldIncludeHalfUndergrounds: boolean;
-  shouldIncludeLofts: boolean;
-  shouldIncludeRooftops: boolean;
+    id: string;
+    priceRange: {
+        deposit?: PriceRange;
+        rent?: PriceRange;
+        /**
+         * 월세 한도 판단에 관리비를 포함할지 여부
+         */
+        shouldIncludeMaintenance: boolean;
+    };
+    bounds: {
+        max: Point;
+        min: Point;
+    };
+    buildingTypes: BuildingType[];
+    roomFloors: RoomFloor[];
+    roomCounts: RoomCount[];
+    contractTypes: ContractType[];
+    shouldIncludeHalfUndergrounds: boolean;
+    shouldIncludeLofts: boolean;
+    shouldIncludeRooftops: boolean;
 }
 
 export function constructFilterQueryParam(filter: Filter) {
-  const { priceRange, bounds, roomFloors, roomCounts, contractTypes } = filter;
+    const { priceRange, bounds, buildingTypes, roomFloors, roomCounts, contractTypes } = filter;
 
-  const tokens: string[] = [
-    `latitude:${bounds.min.lat}~${bounds.max.lat}`,
-    `longitude:${bounds.min.lng}~${bounds.max.lng}`,
-  ];
+    const tokens: string[] = [
+        `latitude:${bounds.min.lat}~${bounds.max.lat}`,
+        `longitude:${bounds.min.lng}~${bounds.max.lng}`,
+    ];
 
-  if (priceRange.rent) {
-    tokens.push(
-      `checkMonth:${priceRange.rent.min || 999}~${priceRange.rent.max}`,
-    );
-  }
+    if (priceRange.rent) {
+        tokens.push(`checkMonth:${priceRange.rent.min || 999}~${priceRange.rent.max}`);
+    }
 
-  if (priceRange.deposit) {
-    tokens.push(
-      `checkDeposit:${priceRange.deposit.min || 999}~${priceRange.deposit.max}`,
-    );
-  }
+    if (priceRange.deposit) {
+        tokens.push(`checkDeposit:${priceRange.deposit.min || 999}~${priceRange.deposit.max}`);
+    }
 
-  if (priceRange.shouldIncludeMaintenance) {
-    tokens.push('isManagerFee;["add"]');
-  }
+    if (priceRange.shouldIncludeMaintenance) {
+        tokens.push('isManagerFee;["add"]');
+    }
 
-  if (roomFloors.length > 0) {
-    const totalRoomFloors = roomFloors.map(f => `"${f}"`);
+    const multipleQueryList: [string, string[]][] = [
+        ['roomCount_etc', roomFloors],
+        ['roomType', roomCounts],
+        ['contractType', contractTypes],
+        ['buildingType', buildingTypes],
+    ];
 
-    tokens.push(`roomCount_etc;[${totalRoomFloors.join(",")}]`);
-  }
+    multipleQueryList.forEach(([label, values]) => {
+        if (values.length > 0) {
+            tokens.push(`${label};[${values.map(t => `"${t}"`).join(',')}]`);
+        }
+    });
 
-  if (roomCounts.length > 0) {
-    tokens.push(`roomType;[${roomCounts.map(t => `"${t}"`).join(",")}]`);
-  }
-
-  if (contractTypes.length > 0) {
-    tokens.push(`contractType;[${contractTypes.map(t => `"${t}"`).join(",")}]`);
-  }
-
-  return tokens.join("||");
+    return tokens.join('||');
 }
