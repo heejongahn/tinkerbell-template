eab12b355d92f1254b94e7ab0279cf7d2b781cdf
Heejong Ahn <heejongahn@gmail.com>
2019-12-07 14:16:04 +0900

    Apply remote change

--- src/remotes/index.ts
+++ src/remotes/index.ts
@@ -1,45 +1,55 @@
-import axios from "axios";
-import { stringify } from "query-string";
+import axios from 'axios';
+import { stringify } from 'query-string';
 
-import { Filter, constructFilterQueryParam } from "../filter";
-import { HousePayload, House } from "../models/House";
+import { Filter, constructFilterQueryParam } from '../filter';
+import { HousePayload, House } from '../models/House';
 
 interface FetchHousesResponse {
-  houses: {
-    direct?: {
-      image: HousePayload[];
+    houses: {
+        direct?: {
+            image?: HousePayload[];
+        };
+        withoutFee?: {
+            image?: HousePayload[];
+        };
+        agency?: {
+            image?: HousePayload[];
+        };
     };
-    agency?: {
-      image: HousePayload[];
-    };
-  };
 }
 
-const apiEndpoint = "https://api.peterpanz.com/houses/area";
+const apiEndpoint = 'https://api.peterpanz.com/houses/area';
 
 export async function fetchHouses(candidate: Filter): Promise<House[]> {
-  const query: { [key: string]: any } = {
-    filter: constructFilterQueryParam(candidate),
-    pageSize: 100,
-    pageIndex: 1,
-  };
+    const query: { [key: string]: any } = {
+        filter: constructFilterQueryParam(candidate),
+        pageSize: 100,
+        pageIndex: 1,
+    };
 
-  const url = `${apiEndpoint}?${stringify(query)}`;
+    const url = `${apiEndpoint}?${stringify(query)}`;
 
-  const { data } = await axios.get<FetchHousesResponse>(url, {
-    headers: { "content-type": "application/json" },
-  });
+    const { data } = await axios.get<FetchHousesResponse>(url, {
+        headers: { 'content-type': 'application/json' },
+    });
 
-  const { direct = { image: [] }, agency = { image: [] } } = data.houses;
+    if (Array.isArray(data)) {
+        return [];
+    }
 
-  const houses = [...direct.image, ...agency.image].filter(h => {
-    return (
-      (candidate.shouldIncludeHalfUndergrounds ||
-        !h.info.is_half_underground) &&
-      (candidate.shouldIncludeLofts || !h.info.is_multilayer) &&
-      (candidate.shouldIncludeRooftops || !h.info.is_octop)
-    );
-  });
+    const { direct, withoutFee, agency } = data.houses;
 
-  return houses.map(payload => new House(payload)).filter(h => h.isNew);
+    const candidates = [direct, withoutFee, agency]
+        .map(category => (category ? category.image || [] : []))
+        .reduce((accm, curr) => accm.concat(curr), []);
+
+    const houses = candidates.filter(h => {
+        return (
+            (candidate.shouldIncludeHalfUndergrounds || !h.info.is_half_underground) &&
+            (candidate.shouldIncludeLofts || !h.info.is_multilayer) &&
+            (candidate.shouldIncludeRooftops || !h.info.is_octop)
+        );
+    });
+
+    return houses.map(payload => new House(payload)).filter(h => h.isNew);
 }
